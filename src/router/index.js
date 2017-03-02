1dea512ea73733546907cdd123e4976cc7fd5292
GeekGhc <ghcgavin@sina.com>
2017-03-02 10:03:16 +0800

    other component

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from 'components/Hello'
 import Start from 'components/Start'
-import Header from 'components/common/SiteHeader'
+import Login from 'components/users/Login'
+import Register from 'components/users/Register'
+import Profile from 'components/users/Profile'
+import Account   from 'components/users/Account'
 
 Vue.use(Router)
 
@@ -10,10 +12,10 @@ export default new Router({
     mode: 'history',
     history: true,
     routes: [
-        {
-            path: '/',
-            name: 'Start',
-            component: Start
-        }
+        {path: '/', name: 'Start', component: Start},
+        {path: '/user/login', name: 'Login', component: Login},
+        {path: '/user/register', name: 'Register', component: Register},
+        {path: '/profile', name: 'Profile', component: Profile},
+        {path: '/account', name: 'Account', component: Account},
     ]
 })
